44a636709d870de0942dd3801a137db4d9045d39
Milad Hassani <miladhassani@gmail.com>
2023-11-02 03:30:36 -0400

    added accounts app and created a superuser

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages'
+    'accounts',
+    # 'pages',
+
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
