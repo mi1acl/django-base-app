5b1246fd5ea03c7c6c1e117213431a99028f085c
Milad Hassani <miladhassani@gmail.com>
2023-11-04 10:47:29 -0400

    added supoort for static files

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,6 @@
 
 from django.urls import path
 from .views import SignupPageView
-from .forms import CustomUserCreationForm
 
 urlpatterns = [
     # Django Admin
--- config/settings.py
+++ config/settings.py
@@ -122,7 +122,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+
+# local development
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+
+# Production static files' root
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_FINDERS = ["django.contrib.staticfiles.finders.FileSystemFinder",
+                       "django.contrib.staticfiles.finders.AppDirectoriesFinder", ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
